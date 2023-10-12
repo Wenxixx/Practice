6ff8d347aa429437eae38385a8cf2712309a3daf
iSteven <coderisteven@gmail.com>
2023-10-12 17:28:45 +0800

    complete

--- Algorithm/LinkList.cpp
+++ Algorithm/LinkList.cpp
@@ -88,12 +88,13 @@ ListNode* deleteDuplicates(ListNode* head) {
    
     ListNode *dummy = new ListNode(0);
     dummy->next = head;
-    
     ListNode* cur = dummy;
     while (cur->next && cur->next->next) {
         if (cur->next->val == cur->next->next->val) {
-            ListNode *next = cur->next->next;
-            cur->next = next->next;
+            int x = cur->next->val;
+            while (cur->next && cur->next->val == x) {
+                cur->next = cur->next->next;
+            }
             
         } else {
             cur = cur->next;
